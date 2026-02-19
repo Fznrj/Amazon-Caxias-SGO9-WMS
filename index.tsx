81c8acf0db5c65843a6e326b03ffab4763d55d54
Fernando Souza <fernando10frango@gmail.com>
2026-02-19 06:03:42 -0300

    Refactor: streamline application initialization by removing StrictMode and logging errors for missing root element.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+console.log('[Entry] Starting React Application...');
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error('[Entry] Root element not found!');
+}
