e81e1abebc3f92f6d54d3aa1ce29aad0da9f31aa
Fernando Souza <fernando10frango@gmail.com>
2026-02-24 22:09:38 -0300

    feat: Add Capacitor for native app integration and configure it.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amazon.caxias.wms',
+  appName: 'Amazon Caxias SGO9',
+  webDir: 'dist'
+};
+
+export default config;
